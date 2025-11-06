dc58d50df15b07cc2d7615bedb47c6a114d2005f
Ramon Zehnder <ramon.zehnder@students.ffhs.ch>
2025-11-06 21:03:08 +0100

    feat: Initial commit for React UI

---
+++ react-ui/src/pages/CharacterSelection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterSelection: React.FC = () => {
+  return (
+    <div>
+      <h1>Character Selection</h1>
+    </div>
+  );
+};
+
+export default CharacterSelection;
